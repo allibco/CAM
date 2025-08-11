7b51eac1e45773eeeb66285e1616faf59d93e249
Allison Baker <abaker@ucar.edu>
2025-08-11 13:29:23 -0600

    modify c bindings

--- src/ionosphere/edyn3d/core/dist_solver_module.F90
+++ src/ionosphere/edyn3d/core/dist_solver_module.F90
@@ -24,8 +24,7 @@ module dist_solver_module
     use mpi_module, only: mpi_rank,dynamo_world,lat_rank,lon_rank,&
                           nmlat_task,nmlon_task,mygrid_size,&
                           sync_mlat_5d, sync_mlon_5d,& 
-                          sync_mlat_3d, sync_mlon_3d, &
-                          superlu_options_t
+                          sync_mlat_3d, sync_mlon_3d
     
 ! the processor grid only covers one hemisphere ((nmlat_h, nmlon)
 ! nmlat_h => # mag latitudes in one hemisphere
@@ -1073,7 +1072,7 @@ module dist_solver_module
          set_default_options_dist, dScalePermstructInit, dLUstructInit, &
          PStatInit, pdgssvx, PStatPrint, Destroy_SuperMatrix_Store_dist, &
          dScalePermstructFree,dDestroy_LU,dLUStructFree,&
-         superlu_gridexit
+         superlu_gridexit, superlu_options_t
     
     use mpi_module,only: lat_size,lon_size,dynamo_world,&
          task_csr_rowstarts, mpi_rank, mygrid_size
