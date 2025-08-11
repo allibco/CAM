bb1f9a5f3fccbd08c1048b5d52738beb0a67ccce
Allison Baker <abaker@ucar.edu>
2025-08-11 11:54:09 -0600

    modify c bindings

--- src/ionosphere/edyn3d/core/dist_solver_module.F90
+++ src/ionosphere/edyn3d/core/dist_solver_module.F90
@@ -1067,7 +1067,12 @@ module dist_solver_module
 
 !-----------------------------------------------------------------------
   function dist_solve_superlu(n_global,n_loc,nnz_loc,rowptr,colind,values,rhs) result(sol)
-    use superlu_mod !binding interfaces
+    use superlu_mod, only: dCreate_CompRowLoc_Matrix_dist, superlu_gridinit, &
+         set_default_options_dist, dScalePermstructInit, dLUstructInit, &
+         PStatInit, pdgssvx, PStatPrint, Destroy_SuperMat_Store_dist, &
+         dScalePermstructFree,dDestroyLU,dLUStructFree,&
+         superlu_gridexit
+    
     use mpi_module,only: lat_size,lon_size,dynamo_world,&
          task_csr_rowstarts, mpi_rank, mygrid_size
     
