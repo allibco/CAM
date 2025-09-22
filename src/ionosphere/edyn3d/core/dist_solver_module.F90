e79a45f6bca4c9bed99459de2066aabad23dc5a6
Allison Baker <abaker@ucar.edu>
2025-09-22 16:32:53 -0600

    non active task mods

--- src/ionosphere/edyn3d/core/dist_solver_module.F90
+++ src/ionosphere/edyn3d/core/dist_solver_module.F90
@@ -951,94 +951,95 @@ module dist_solver_module
 
     !SET UP BLOCK CSR
     if (mpi_size > 1) then
-       !now set up my block of the csr matrix (my_rowptr, my_values, my_colind)
-       !still using 1-based indices
-       my_rowptr(1) = 1
+       if (mpi_rank >= 0) then !active proc
+          !now set up my block of the csr matrix (my_rowptr, my_values, my_colind)
+          !still using 1-based indices
+          my_rowptr(1) = 1
 
-       ! now swap hemisphere data with partner
-       !for N and S hemi, the even proc rows go first to maintain grid order
-       !(so the south owning process)
-       if (mod(mpi_rank,2) == 0) then !EVEN, own south, **send north**
-          call partner_exchange_hemisphere_mat(MAX_NNZ, rowptr_n, values_n, colind_n, &
-               partner_rowptr, partner_values, partner_cols)
+          ! now swap hemisphere data with partner
+          !for N and S hemi, the even proc rows go first to maintain grid order
+          !(so the south owning process)
+          if (mod(mpi_rank,2) == 0) then !EVEN, own south, **send north**
+             call partner_exchange_hemisphere_mat(MAX_NNZ, rowptr_n, values_n, colind_n, &
+                  partner_rowptr, partner_values, partner_cols)
+             
+             !my south (goes first into csr block)
+             if (num_row_s + 1 > size(my_rowptr)) then
+                write(iulog,*) 'AB: Error: my_rowptr array too small for south data, num_row_s +1, size = ', num_row_s+1, size(my_rowptr)
+             endif
+             do concurrent (i = 2:num_row_s + 1)
+                my_rowptr(i) = rowptr_s(i)
+                !write(iulog,*) 'AB: mpi_rank, i, my_rowptr(i), rowwptr_s(i)', mpi_rank, i, my_rowptr(i), rowptr_s(i)
+             enddo
+             !nnz_south set above - but double check
+             if (nnz_south /= my_rowptr(num_row_s + 1) - 1) then
+                write(iulog,*) 'AB: Error mpi_rank, nnz_south = ', mpi_rank, nnz_south
+             endif
+          
+             write(iulog,*) 'AB: mpi_rank, num_row_s, nnz_south = ', mpi_rank, num_row_s, nnz_south
+             if (nnz_south > size(my_colind) .or. nnz_south > size(my_values)) then
+                write(iulog,*) 'AB: Error: my_colind or my_values array too small for south data'
+             endif
+          
+             do concurrent (i = 1:nnz_south)
+                my_colind(i) = colind_s(i)
+                my_values(i) = values_s(i)
+             enddo
+          
+             !partner has north
+             do i = 1,  partner_hgridsize
+                cnt = partner_rowptr(i+1) - partner_rowptr(i)
+                my_rowptr(num_row_s + 1 + i) = my_rowptr(num_row_s + i) + cnt
+             enddo
+             !north proc's data goes second
+             !CHECK mygrid_size = num_row_s + partner_hgridsize
+             if (mygrid_size /= num_row_s + partner_hgridsize) then
+                write(iulog, *) 'AB: Error: mygrid_size & partner_hgridsize check', mygrid_size, partner_hgridsize
+             endif
+          
+             !nnz_north needs to be set from partner's info
+             nnz_north = my_rowptr(mygrid_size + 1) -1  - nnz_south
+          
+             if (nnz_south + nnz_north > size(my_colind)) then
+                write(iulog,*) 'AB: Error: my_colind array too small for combined data: mpi_rank, nnz_south, nnz_north, size', mpi_rank, nnz_south, nnz_north, size(my_colind)
+             endif
+          
+             do concurrent (i = 1:nnz_north)
+                my_colind(nnz_south + i) = partner_cols(i)
+                my_values(nnz_south + i) = partner_values(i)
+             enddo
+          
+          else !ODD, own north, **send south**
+             call partner_exchange_hemisphere_mat(MAX_NNZ, rowptr_s, values_s, colind_s, &
+                  partner_rowptr, partner_values, partner_cols)
+          
+             !partner has south - partner's data goes first into csr block
+             do concurrent (i = 2:partner_hgridsize + 1)
+                my_rowptr(i) = partner_rowptr(i)
+             enddo
 
-          !my south (goes first into csr block)
-          if (num_row_s + 1 > size(my_rowptr)) then
-             write(iulog,*) 'AB: Error: my_rowptr array too small for south data, num_row_s +1, size = ', num_row_s+1, size(my_rowptr)
+             !calc based on partner info
+             nnz_south = my_rowptr(partner_hgridsize + 1) -1
+
+             do concurrent (i = 1:nnz_south)
+                my_colind(i) = partner_cols(i)
+                my_values(i) = partner_values(i)
+             enddo
+          
+             !my north - goes second
+             do i = 1, num_row_n
+                cnt = rowptr_n(i+1) - rowptr_n(i)
+                my_rowptr( partner_hgridsize + i + 1) = my_rowptr(partner_hgridsize + i) + cnt
+             enddo
+          
+             !nnz_north already calculated for me above
+
+             do concurrent (i = 1:nnz_north)
+                my_colind(nnz_south + i) = colind_n(i)
+                my_values(nnz_south + i) = values_n(i)
+             enddo
           endif
-          do concurrent (i = 2:num_row_s + 1)
-             my_rowptr(i) = rowptr_s(i)
-             !write(iulog,*) 'AB: mpi_rank, i, my_rowptr(i), rowwptr_s(i)', mpi_rank, i, my_rowptr(i), rowptr_s(i)
-          enddo
-          !nnz_south set above - but double check
-          if (nnz_south /= my_rowptr(num_row_s + 1) - 1) then
-             write(iulog,*) 'AB: Error mpi_rank, nnz_south = ', mpi_rank, nnz_south
-          endif
-          
-          write(iulog,*) 'AB: mpi_rank, num_row_s, nnz_south = ', mpi_rank, num_row_s, nnz_south
-          if (nnz_south > size(my_colind) .or. nnz_south > size(my_values)) then
-             write(iulog,*) 'AB: Error: my_colind or my_values array too small for south data'
-          endif
-          
-          do concurrent (i = 1:nnz_south)
-             my_colind(i) = colind_s(i)
-             my_values(i) = values_s(i)
-          enddo
-          
-          !partner has north
-          do i = 1,  partner_hgridsize
-             cnt = partner_rowptr(i+1) - partner_rowptr(i)
-             my_rowptr(num_row_s + 1 + i) = my_rowptr(num_row_s + i) + cnt
-          enddo
-          !north proc's data goes second
-          !CHECK mygrid_size = num_row_s + partner_hgridsize
-          if (mygrid_size /= num_row_s + partner_hgridsize) then
-             write(iulog, *) 'AB: Error: mygrid_size & partner_hgridsize check', mygrid_size, partner_hgridsize
-          endif
-          
-          !nnz_north needs to be set from partner's info
-          nnz_north = my_rowptr(mygrid_size + 1) -1  - nnz_south
-          
-          if (nnz_south + nnz_north > size(my_colind)) then
-             write(iulog,*) 'AB: Error: my_colind array too small for combined data: mpi_rank, nnz_south, nnz_north, size', mpi_rank, nnz_south, nnz_north, size(my_colind)
-          endif
-
-          do concurrent (i = 1:nnz_north)
-             my_colind(nnz_south + i) = partner_cols(i)
-             my_values(nnz_south + i) = partner_values(i)
-          enddo
-          
-       else !ODD, own north, **send south**
-          call partner_exchange_hemisphere_mat(MAX_NNZ, rowptr_s, values_s, colind_s, &
-               partner_rowptr, partner_values, partner_cols)
-          
-          !partner has south - partner's data goes first into csr block
-          do concurrent (i = 2:partner_hgridsize + 1)
-             my_rowptr(i) = partner_rowptr(i)
-          enddo
-
-          !calc based on partner info
-          nnz_south = my_rowptr(partner_hgridsize + 1) -1
-
-          do concurrent (i = 1:nnz_south)
-             my_colind(i) = partner_cols(i)
-             my_values(i) = partner_values(i)
-          enddo
-          
-          !my north - goes second
-          do i = 1, num_row_n
-             cnt = rowptr_n(i+1) - rowptr_n(i)
-             my_rowptr( partner_hgridsize + i + 1) = my_rowptr(partner_hgridsize + i) + cnt
-          enddo
-          
-          !nnz_north already calculated for me above
-
-          do concurrent (i = 1:nnz_north)
-             my_colind(nnz_south + i) = colind_n(i)
-             my_values(nnz_south + i) = values_n(i)
-          enddo
-          
-       endif
+       endif! active tasks
     else !one task
        !south 
        do concurrent (i = 1:num_row_s + 1)
