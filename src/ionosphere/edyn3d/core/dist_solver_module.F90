7cfcee70700f361d7f601b739114bcf9fa81af2a
Allison Baker <abaker@ucar.edu>
2025-12-03 12:42:12 -0700

    declaration missing

--- src/ionosphere/edyn3d/core/dist_solver_module.F90
+++ src/ionosphere/edyn3d/core/dist_solver_module.F90
@@ -1220,7 +1220,7 @@ module dist_solver_module
     use params_module,only:nmlat_h,nmlat_T1,nmlon
     use cons_module,only:phi_pol
     use mpi_module, only:mlatd0, mlatd1, mlat0, mlat1, mpi_rank, &
-         un_mpi_rank, un_mpi_size, &
+         un_mpi_rank, un_mpi_size, ex_mpi_rank, ex_mpi_size, &
          mlon0, mlon1, mlond0, mlond1, &
          lat_rank, lon_rank, &
          ij_start_s, ij_stop_s, ij_start_n, ij_stop_n, &
