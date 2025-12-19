1e088ee05d881fe387954561d935e1060e542be8
Allison Baker <abaker@ucar.edu>
2025-12-19 13:29:47 -0700

    changes to superlu to reuse sparsity pattern - fixed

--- src/ionosphere/edyn3d/core/dist_solver_module.F90
+++ src/ionosphere/edyn3d/core/dist_solver_module.F90
@@ -191,12 +191,12 @@ module dist_solver_module
        !free later (only after done because we are just initializing once)
        !call dist_spmv_free(halo)
 
-       write(*,*) 'Dist_ls: Finished spmv'
+       !write(*,*) 'Dist_ls: Finished spmv'
           
        ! reconstruct 2D distribution of FAC based on z
        fac_hl(:,mlat0:mlat1,mlon0:mlon1) = dist_unravel(z)
 
-       write(*,*) 'Dist_ls: Finished unravel'
+       !write(*,*) 'Dist_ls: Finished unravel'
        
        !get ghost/halo points (only dynamo procs)
        if (mpi_rank >=0 ) then
@@ -214,7 +214,7 @@ module dist_solver_module
        endif !dynamo procs
      endif !FAC
 
-     write(*,*) 'Dist_ls: Finished fac section'
+     !write(*,*) 'Dist_ls: Finished fac section'
      
      ! add FAC forcing to RHS
      !(these are both set for contiguous rows already - all union procs own)
@@ -241,7 +241,7 @@ module dist_solver_module
      sol = dist_solve_superlu(nlonlat, mygrid_size, nnz, g_rowptr, g_colind(1:nnz), g_values_csr(1:nnz), rhs)
      call t_stopf('linear_system->solve_superlu')
 
-     print *, 'Dist_ls: done with solve'
+     !print *, 'Dist_ls: done with solve'
 
      
      ! reconstruct 2D distribution of potential based on the solution
@@ -263,7 +263,7 @@ module dist_solver_module
         endif
      endif
      
-     print *, 'Dist_ls: done with unravel'
+     !print *, 'Dist_ls: done with unravel'
 
      !if output turned on for debugging 
      if (output_matrix == .true.) then
@@ -1309,7 +1309,7 @@ module dist_solver_module
 
     if (.not. superlu_initialized) then
 
-       print *, 'initializing superlu ...'
+       !print *, 'initializing superlu ...'
 
        ! Create Fortran handles for the C structures used in SuperLU_DIST
        call f_create_gridinfo_handle(grid)
